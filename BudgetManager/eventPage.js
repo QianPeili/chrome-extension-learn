f553e6fc69093734cd9dcf142bb81d085ee4aa52
QianPeili <qianperry@outlook.com>
2018-03-25 09:15:22 +0800

    context menu functionality

--- BudgetManager/eventPage.js
+++ BudgetManager/eventPage.js
@@ -1,6 +1,39 @@
-var contextMenuItem = {
+var spendMoneyItem = {
     "id": "spenMoney",
     "title": "SpendMoney",
     "contexts": ["selection"]
 };
-chrome.contextMenus.create(contextMenuItem);
+chrome.contextMenus.create(spendMoneyItem);
+
+var isInt = function(value) {
+    return !isNaN(value) &&
+           parseInt(Number(value)) == value &&
+           !isNaN(parseInt(value, 10));
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == spendMoneyItem.id && clickData.selectionText) {
+        if (isInt(clickData.selectionText)) {
+            chrome.storage.sync.get(["total", "limit"], function(budget){
+                var newTotal = 0;
+                if (budget.total) {
+                    newTotal += parseInt(budget.total);
+                }
+    
+                newTotal += parseInt(clickData.selectionText);
+                chrome.storage.sync.set({"total": newTotal}, function(){
+                    if (newTotal >= budget.limit) {
+                        var notifOptions = {
+                            type: "basic", 
+                            iconUrl: "icon48.png",
+                            title: "Limit reached!",
+                            message: "Uh oh! Looks like you reached your limit."
+                        };
+                        chrome.notifications.create("limitNotif", notifOptions);
+                    }
+                });
+                $("#total").text(newTotal);
+            });
+        };
+    };
+});
