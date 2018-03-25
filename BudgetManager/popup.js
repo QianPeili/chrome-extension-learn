7d37370f130e430b8ff00e4669e5c80ac1d053ef
QianPeili <qianperry@outlook.com>
2018-03-25 08:55:24 +0800

    notification

--- BudgetManager/popup.js
+++ BudgetManager/popup.js
@@ -5,7 +5,7 @@ $(function(){
     });
 
     $("#spendAmount").click(function(){
-        chrome.storage.sync.get("total", function(budget){
+        chrome.storage.sync.get(["total", "limit"], function(budget){
             var newTotal = 0;
             if (budget.total) {
                 newTotal += parseInt(budget.total);
@@ -15,8 +15,17 @@ $(function(){
             if (amount) {
                 newTotal += parseInt(amount);
             }
-
-            chrome.storage.sync.set({"total": newTotal});
+            chrome.storage.sync.set({"total": newTotal}, function(){
+                if (amount && newTotal >= budget.limit) {
+                    var notifOptions = {
+                        type: "basic", 
+                        iconUrl: "icon48.png",
+                        title: "Limit reached!",
+                        message: "Uh oh! Looks like you reached your limit."
+                    };
+                    chrome.notifications.create("limitNotif", notifOptions);
+                }
+            });
             $("#total").text(newTotal);
             $("#amount").val("");
         });
