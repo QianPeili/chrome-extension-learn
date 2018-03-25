3db9ae86f70438d3843835c51bbb162cb3a0728f
QianPeili <qianperry@outlook.com>
2018-03-25 21:56:01 +0800

    visit manmabuy with current url

--- InAction/manmanbuy/eventPage.js
+++ InAction/manmanbuy/eventPage.js
@@ -1,3 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    alert("hello");
+    var mmbUrl = "http://tool.manmanbuy.com/historyLowest.aspx?url=" + 
+                 encodeURIComponent(tab.url);
+    var newTab = {
+        "url": mmbUrl
+    }
+    chrome.tabs.create(newTab);
 });
