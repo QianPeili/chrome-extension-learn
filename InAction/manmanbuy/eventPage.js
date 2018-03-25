76a5cf9a812172b03caa2e8ab2bc814957d2409d
QianPeili <qianperry@outlook.com>
2018-03-25 22:00:16 +0800

    manmanbuy with selectionText

--- InAction/manmanbuy/eventPage.js
+++ InAction/manmanbuy/eventPage.js
@@ -6,3 +6,23 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     }
     chrome.tabs.create(newTab);
 });
+
+var menuItem = {
+    "id": "searchInManmanBuy",
+    "title": "manmanbuy",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == menuItem.id && clickData.selectionText){
+        var mmbUrl = "http://s.manmanbuy.com/Default.aspx?key=" +
+                      encodeURIComponent(clickData.selectionText);
+        var createData = {
+            "url": mmbUrl,
+        };
+        chrome.tabs.create(createData, function(){});
+    };
+});
